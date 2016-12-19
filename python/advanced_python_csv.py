3220aa25a6e012b66d7e75b28d24e17bc8f9989c
kristikwon <kristi.kwon11@gmail.com>
2016-12-19 04:09:19 -0800

    python code for part II

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,12 @@
-PLACE YOUR CODE HERE
+# Part II: Write to CSV File
+# create emails.csv file that contains email addresses from Part I
+
+import advanced_python_regex
+import csv
+
+df = advanced_python_regex.read_data()
+data = advanced_python_regex.email_address(df)
+
+c = csv.writer(open('emails.csv','w'))
+for item in data:
+  c.writerow([item])
